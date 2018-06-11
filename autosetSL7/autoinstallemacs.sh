017604e401ec71fcd9ebeb4d2eea98648c5a0534
wuhongyi <81845742@qq.com>
2018-06-11 09:58:15 +0800

    update default emacs 26.1

--- autosetSL7/autoinstallemacs.sh
+++ autosetSL7/autoinstallemacs.sh
@@ -5,12 +5,12 @@
 # Author: Hongyi Wu(吴鸿毅)
 # Email: wuhongyi@qq.com 
 # Created: 五 6月  2 10:13:19 2017 (+0800)
-# Last-Updated: 六 3月 17 15:58:05 2018 (+0800)
+# Last-Updated: 一 6月 11 09:50:24 2018 (+0800)
 #           By: Hongyi Wu(吴鸿毅)
-#     Update #: 12
+#     Update #: 13
 # URL: http://wuhongyi.cn 
 
-emacsfilename="emacs-25.3"  #"emacs-25.2"
+emacsfilename="emacs-26.1"  #"emacs-25.3"  #"emacs-25.2"
 
 if [ `whoami` = "root" ];then 
     echo "当前为root用户，能够执行此脚本！" 
