4bfe1bff3d1ac589df74ff3c01a88cde53f2d29f
wuhongyi <wuhongyi@pku.edu.cn>
2019-03-13 19:33:37 +0800

    20190313

--- PrivateSettings/dash.sh
+++ PrivateSettings/dash.sh
@@ -5,9 +5,9 @@
 # Author: Hongyi Wu(吴鸿毅)
 # Email: wuhongyi@qq.com 
 # Created: 六 2月  9 17:16:27 2019 (+0800)
-# Last-Updated: 二 3月 12 23:27:15 2019 (+0800)
+# Last-Updated: 三 3月 13 12:24:09 2019 (+0800)
 #           By: Hongyi Wu(吴鸿毅)
-#     Update #: 2
+#     Update #: 7
 # URL: http://wuhongyi.cn 
 
 
@@ -17,6 +17,8 @@ else
     echo "请在ROOT权限下执行此脚本！！！"
     exit 1
 fi
+
+yum -y install nodejs.x86_64  ##update to >= 8.0, yarn require
 
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 yum -y install yarn
@@ -29,7 +31,8 @@ pip install dash==0.36.0
 pip install dash-html-components==0.13.5
 pip install dash-core-components==0.43.0
 pip install dash-table==3.1.11
-pip install dash-daq==0.1.0 
+pip install dash-daq==0.1.4
+#pip install Phidget22
 
 # 
 # dash.sh ends here
